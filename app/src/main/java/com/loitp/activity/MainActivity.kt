1963752e66b1cbcab8c4dcbfb0296aa198eab629
v.loitp <v.Loitp@cloudhms.net>
2020-08-14 13:29:51 +0700

    update pdf

--- app/src/main/java/com/loitp/activity/MainActivity.kt
+++ app/src/main/java/com/loitp/activity/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.loitp.activity
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.Gravity
@@ -9,9 +8,12 @@ import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import com.core.base.BaseFontActivity
-import com.core.common.Constants
-import com.core.helper.gallery.GalleryCoreSplashActivity
-import com.core.utilities.*
+import com.core.utilities.LImageUtil
+import com.core.utilities.LScreenUtil
+import com.core.utilities.LSocialUtil
+import com.core.utilities.LUIUtil
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle
+import com.github.barteksc.pdfviewer.util.FitPolicy
 import com.google.android.material.navigation.NavigationView
 import com.loitp.R
 import com.loitp.fragment.HomeFragment
@@ -54,10 +56,56 @@ class MainActivity : BaseFontActivity(), NavigationView.OnNavigationItemSelected
 
         //cover
         LImageUtil.load(context = activity, url = getString(R.string.link_cover), imageView = navViewStart.getHeaderView(0).ivCover)
-
-        //pdf
-
+        showPDF()
         switchHomeScreen()
+    }
+
+    private fun showPDF() {
+        val inputStream = assets.open("Fossil_Android_TrackMe.pdf")
+        pdfView.fromStream(inputStream)
+                //.pages(0) // all pages are displayed by default
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .onLoad { nbPages ->
+                    logD("loadComplete $nbPages")
+                } // called after document is loaded and starts to be rendered
+                .onPageChange { page, pageCount ->
+                    logD("onPageChange $page/$pageCount")
+                }
+                .onPageScroll { page, positionOffset ->
+                    logD("onPageScrolled $page - $positionOffset")
+                }
+                .onError { t ->
+                    logE("onError $t")
+                }
+                .onPageError { page, t ->
+                    logE("onPageError $page -> $t")
+                }
+                .onRender { nbPages ->
+                    logD("onInitiallyRendered nbPages $nbPages")
+                } // called after document is rendered for the first time
+                // called on single tap, return true if handled, false to toggle scroll handle visibility
+                .onTap { _ ->
+                    logD("onTap")
+                    false
+                }
+                .onLongPress { _ ->
+                    logD("OnLongPressListener")
+                }
+                .enableAnnotationRendering(true) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(DefaultScrollHandle(this))
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .autoSpacing(true) // add dynamic spacing to fit each page on its own on the screen
+                .pageFitPolicy(FitPolicy.WIDTH)
+                .pageSnap(true) // snap pages to screen boundaries
+                .pageFling(true) // make a fling change only a single page like ViewPager
+                .nightMode(false) // toggle night parrallaxMode
+                .load()
     }
 
     private fun switchHomeScreen() {
